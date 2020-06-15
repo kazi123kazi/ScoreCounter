495eb3e0aa3288bcce305b7572376d2921b9904e
KAZI SAHARIAR RAHI <srahi00000@gmail.com>
2020-06-15 21:19:23 +0530

    Reviewed necessary changes added

--- app/src/main/java/com/example/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/scorecounter/MainActivity.java
@@ -6,102 +6,106 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+/* Author : Kazi Sahariar Rahi
+*All rights are reserved.
+* Open source code.
+* Source code can be found in my github repo
+* github.com/kazi123kazi
+ */
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView teamAScore,teamBScore;
-    TextView teamAWicket,teamBWicket;
-    int scoreA = 0,scoreB = 0;
-    int wicketA = 0,wicketB = 0;
-
+    private TextView teamAScore,teamBScore,teamAWicket,teamBWicket;
+    int scoreA = 0,scoreB = 0,wicketA = 0,wicketB = 0;;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initwidget();
+    }
+
+    private void initwidget() {
         teamAScore = (TextView) findViewById(R.id.teamAScore);
         teamAWicket = (TextView) findViewById(R.id.teamAWicket);
         teamBScore = (TextView) findViewById(R.id.teamBScore);
         teamBWicket = (TextView) findViewById(R.id.teamBWicket);
     }
-    public void displayScoreA()
+
+    //Display
+    private void displayScoreA()
     {
         teamAScore.setText(String.valueOf(scoreA));
     }
-    public void displayWicketA()
+    private void displayWicketA()
     {
         teamAWicket.setText(String.valueOf(wicketA));
     }
-    public void displayScoreB()
+    private void displayScoreB()
     {
         teamBScore.setText(String.valueOf(scoreB));
     }
-
-    public void displayWicketB()
+    private void displayWicketB()
     {
         teamBWicket.setText(String.valueOf(wicketB));
     }
 
-
-
-
+   //Player A
     public void scoreOneA(View view){
-        scoreA = scoreA+1;
+        scoreA += 1;
         displayScoreA();
     }
-
     public void scoreTwoA(View view){
-        scoreA = scoreA+2;
+        scoreA += 2;
         displayScoreA();
     }
     public void scoreThreeA(View view){
-        scoreA = scoreA+3;
+        scoreA += 3;
         displayScoreA();
     }
     public void scoreFourA(View view){
-        scoreA = scoreA+4;
+        scoreA += 4;
         displayScoreA();
     }
     public void scoreSixA(View view){
-        scoreA = scoreA+6;
+        scoreA += 6;
         displayScoreA();
     }
     public void wicketA(View view){
-        wicketA = wicketA+1;
+        wicketA += 1;
         displayWicketA();
     }
 
 
 
 
-
+    //Player B
     public void scoreOneB(View view){
-        scoreB = scoreB+1;
+        scoreB +=1;
         displayScoreB();
     }
-
     public void scoreTwoB(View view){
-        scoreB = scoreB+2;
+        scoreB +=2;
         displayScoreB();
     }
     public void scoreThreeB(View view){
-        scoreB = scoreB+3;
+        scoreB += 3;
         displayScoreB();
     }
     public void scoreFourB(View view){
-        scoreB = scoreB+4;
+        scoreB += 4;
         displayScoreB();
     }
     public void scoreSixB(View view){
-        scoreB = scoreB+6;
+        scoreB += 6;
         displayScoreB();
     }
     public void wicketB(View view){
-        wicketB = wicketB+1;
+        wicketB +=1;
         displayWicketB();
     }
 
 
-
+   //Reset
     public void resetA(View view){
         scoreA = 0;wicketA = 0;
         displayScoreA();displayWicketA();
